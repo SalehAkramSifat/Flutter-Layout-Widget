1c030b352569036a4ac6003027f72dbf129fe3ff
Saleh Akram Sifat <salehakramsifat@gmail.com>
2025-02-10 15:05:54 +0600

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,22 +19,44 @@ class HomeActivity extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Layout Widget",),centerTitle: true,backgroundColor: Colors.blue,),
-    body: Padding(padding: EdgeInsets.all(10),
+    return Scaffold(
+      appBar: AppBar(title: Text("Layout Widget",),centerTitle: true,backgroundColor: Colors.blue,),
+    body: SingleChildScrollView(padding: EdgeInsets.all(10),
     child: Column(crossAxisAlignment: CrossAxisAlignment.start,children: [
       Container(height: 200,width: double.infinity, decoration: BoxDecoration(color: Colors.blueAccent,borderRadius: BorderRadius.circular(20),),alignment: Alignment.center,child: Text("It's Container Layout", style: TextStyle(fontSize: 30, color: Colors.white),),),
 
       SizedBox(height: 20,),
       Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,children: [
-        Container(width: 100,height: 100, decoration:BoxDecoration(color: Colors.blueAccent, borderRadius: BorderRadius.circular(20))),
+        Container(width: 100,height: 100, decoration:BoxDecoration(color: Colors.blueAccent, borderRadius: BorderRadius.circular(20),),),
         Container(width: 100,height: 100, decoration:BoxDecoration(color: Colors.purpleAccent, borderRadius: BorderRadius.circular(20),)),
         Container(width: 100,height: 100, decoration:BoxDecoration(color: Colors.deepOrangeAccent, borderRadius: BorderRadius.circular(20)),),
-        Container(width: 100,height: 100, decoration:BoxDecoration(color: Colors.lightGreenAccent, borderRadius: BorderRadius.circular(20)),),],)
-    ],),),
+        Container(width: 100,height: 100, decoration:BoxDecoration(color: Colors.lightGreenAccent, borderRadius: BorderRadius.circular(20)),),],),
+
+      SizedBox(height: 10,),
+      Column(crossAxisAlignment: CrossAxisAlignment.start,children: [
+        Text("Column Widget 1", style: TextStyle(fontSize: 20),),
+        SizedBox(height: 10,),
+        Text("Column Widget 2", style: TextStyle(fontSize: 20),),
+      ],),
+
+      SizedBox(height: 20,),
+      Stack(children: [
+        Container(height: 100, width: double.infinity, color: Colors.blue,),
+        Positioned(top: 20, left: 20, child: Container(width: 50,height: 50,color: Colors.yellow,),),
+        Positioned(top: 20, left: 80, child: Container(width: 50, height: 50,color: Colors.yellow,),),
+        Positioned(top: 20, left: 140, child: Container(width: 50, height: 50,color: Colors.yellow,),),
+        Positioned(top: 20, left: 210, child: Container(width: 50, height: 50,color: Colors.yellow,),),
+        Positioned(top: 20, left: 270, child: Container(width: 50, height: 50, color: Colors.yellow,),),
+        Positioned(top: 20, left: 330, child: Container(width: 50, height: 50, color: Colors.yellow,),)
+      ],
 
 
 
-    );
+      ),],),
+
+
+
+    ));
 
 
   }
